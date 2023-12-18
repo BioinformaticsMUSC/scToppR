719233008c4e5b650d84d53a50fcf6dddae1c314
Bryan Granger <grangerb@musc.edu>
2023-12-18 14:26:40 -0500

    big update 0.1.0

--- R/TOPP_GENE.R
+++ R/TOPP_GENE.R
@@ -2,6 +2,9 @@
 #'
 #' @param markers A vector of markers or dataframe with columns as cluster labels
 #' @param topp_categories A string or vector with specific toppfun categories for the query
+#' @param cluster_col Column name for the groups of cells (e.g. cluster or celltype)
+#' @param gene_col Column name for genes (e.g. gene or feature)
+#' @param num_genes Number of genes per group to use for toppGene query (default = 20)
 #' @param key_type Gene name format
 #' @param p_value P-value cutoff for results
 #' @param min_genes Minimum number of genes to match in a query
@@ -11,57 +14,95 @@
 #' @importFrom stringr str_glue str_c
 #' @return data.frame
 #' @examples
-#' toppFun(markers=c("IFNG", "FOXP3"), topp_categories="GeneOntologyBiologicalProcess", key_type="SYMBOL")
+#' toppFun(markers=marker_table, topp_categories="GeneOntologyBiologicalProcess", key_type="SYMBOL")
 #' @export
 toppFun <- function(markers,
-                    topp_categories,
+                    topp_categories = NULL,
+                    cluster_col = "cluster",
+                    gene_col = "gene",
+                    num_genes = 20,
+                    do_log = TRUE,
+                    FDR_cutoff = 5,
+                    clusters = NULL,
+                    correction="FDR",
                     key_type = "SYMBOL",
                     p_value= 0.05,
                     min_genes=1,
                     max_genes=1500,
-                    max_results=10,
-                    correction="FDR") {
+                    max_results=50
+                    ) {
+
+  #subset clusters if needed
+  if (!(is.null(clusters))) {
+    markers <- markers |>
+      dplyr::filter(!!as.name(cluster_col) %in% clusters)
+  }
+
   #parse input
-  if (class(markers) %in% c("list", "character")) {
-    markers = list(markers=markers)
-  } else if (class(markers) != 'data.frame') {
-    stop("Cannot parse list/table of markers.")
+  if ('data.frame' %in% class(markers)) {
+    marker_table <- process_markers(markers=markers,
+                                    cluster_col=cluster_col,
+                                    gene_col=gene_col,
+                                    num_genes=num_genes,
+                                    FDR_cutoff=FDR_cutoff,
+                                    avg_logFC_col="avg_logFC",
+                                    p_val_col="p_val",
+                                    adj_p_val_col="p_val_adj")
+  } else {
+    stop("data format not recognized")
+  }
+
+  #parse correction method
+  if (!(correction %in% c("none", "FDR", "Bonferroni"))) {
+    stop("invalid P-value correction method, please select either 'none', 'FDR', or 'Bonferroni'")
+  }
+
+  #parse categories
+  if (is.null(topp_categories)) {
+    topp_categories = get_ToppCats()
   }
 
   big_df <- data.frame()
   missing_clusters = c()
 
-  for (col in names(markers)) {
-
+  for (col in names(marker_table)) {
+    #print(col)
     if (!(col %in% c('rank', 'X'))) {
-      if (length(names(markers)) > 1) {
-        cat('Working on cluster:', col, "\n")
+      gene_list = marker_table[[col]]
+      #print(gene_list)
+      if (sum(!(is.na(gene_list))) >= min_genes) {
+        cat('Working on cluster:', col, '\n')
+        d <- get_topp(gene_list = gene_list,
+                      topp_categories = topp_categories,
+                      key_type = "SYMBOL",
+                      p_value=p_value,
+                      min_genes=min_genes,
+                      max_genes=max_genes,
+                      max_results=max_results,
+                      correction=correction)
+        #print(d)
+        if (nrow(d) == 0){
+          missing_clusters = append(missing_clusters, col)
+        } else if (length(names(markers)) == 1) {
+          big_df <- rbind(big_df, d)
+        } else {
+          d[['Cluster']] = col
+          big_df <- rbind(big_df, d)
+        }
       } else {
-        cat('Working on list of markers\n')
-      }
-      gene_list = markers[[col]]
-      d <- get_topp(gene_list = gene_list,
-                    topp_categories = topp_categories,
-                    key_type = "SYMBOL",
-                    p_value=p_value,
-                    min_genes=min_genes,
-                    max_genes=max_genes,
-                    max_results=max_results,
-                    correction=correction)
-      if (nrow(d) == 0){
         missing_clusters = append(missing_clusters, col)
-      } else if (length(names(markers)) == 1) {
-        big_df <- rbind(big_df, d)
-      } else {
-        d[['Cluster']] = col
-        big_df <- rbind(big_df, d)
       }
     }
-
+  }
+  #print(big_df)
+  #calculate -log10(FDR)
+  if (isTRUE(do_log)){
+    big_df <- big_df |>
+      dplyr::mutate(nlog10_fdr = -log10(QValueFDRBH))
   }
   #report any missing clusters
   if (length(missing_clusters) > 0) {
-    write(stringr::str_glue("WARNING: no results found for clusters {stringr::str_c(missing_clusters, collapse=',')}"),
+    write(stringr::str_glue("WARNING: no results found for clusters {stringr::str_c(missing_clusters, collapse=', ')}"),
           stderr())
   }
   return (big_df)
@@ -87,6 +128,52 @@ get_Entrez<- function(genes){
     new_gene_list <- base::append(new_gene_list, g[['Entrez']])
   }
   return (new_gene_list)
+}
+
+process_markers <- function (markers, cluster_col, gene_col,
+                             num_genes=20,
+                             FDR_cutoff=5,
+                             avg_logFC_col="avg_logFC",
+                             p_val_col="p_val",
+                             adj_p_val_col="p_val_adj") {
+
+
+  #parse columns
+  #print(colnames(markers))
+  if (!(avg_logFC_col %in% colnames(markers))) {
+    avg_logFC_col = stringr::str_subset(colnames(markers),
+                                        pattern="avg")
+
+  }
+  if (!(adj_p_val_col %in% colnames(markers))) {
+    adj_p_val_col = stringr::str_subset(colnames(markers),
+                                        pattern="adj")
+  }
+
+  #avg_foldfc
+  marker_table <- data.frame(row.names=seq(1, num_genes))
+  for (cl in unique(markers[[cluster_col]])) {
+    tdf <- markers |>
+      dplyr::filter(!!as.name(cluster_col) == cl) |>
+      #mutate(abs = abs(avg_log2FC)) |>
+      dplyr::arrange(-!!as.name(avg_logFC_col)) |>
+      dplyr::mutate(log = -log10(!!as.name(adj_p_val_col))) |>
+      dplyr::filter(log > FDR_cutoff) |>
+      head(num_genes) |>
+      dplyr::select(!!as.name(gene_col))
+    if (nrow(tdf) < num_genes) {
+      new_tdf <- data.frame(row.names=1:num_genes)
+      new_tdf$gene <- NA
+      new_tdf[1:nrow(tdf),"gene"] = tdf[1:nrow(tdf), "gene"]
+      tdf <- new_tdf
+    }
+
+    colnames(tdf) <- cl
+    #rownames(tdf) <- seq(1:nrow(tdf))
+    marker_table <- dplyr::bind_cols(marker_table, tdf)
+  }
+  #print(marker_table)
+  return (marker_table)
 }
 
 get_topp <- function(gene_list,
@@ -182,3 +269,119 @@ get_ToppCats <- function() {
                 "Disease")
   return (toppCats)
 }
+
+#' Save toppData results (optionally) split by celltype/cluster
+#'
+#' @param toppData Results from toppFun as a dataframe
+#' @param filename filename prefix for each split file
+#' @param split Boolean, whether to split the dataframe by celltype/cluster
+#' @param format Saved file format, one of c("xlsx", "csv", "tsv")
+#' @examples
+#' toppSave(toppData, filename="toppFun_results", split = TRUE, format = "xlsx")
+#' @export
+toppSave <- function (toppData,
+                      filename = NULL,
+                      split = TRUE,
+                      format = "xlsx") {
+  if (isTRUE(split)) {
+    for (gr in unique(toppData$Cluster)) {
+      tmp_toppData <- toppData |>
+        dplyr::filter(Cluster == gr)
+
+      if (format == 'xlsx') {
+
+        if (is.null(filename)) {
+          this_file = stringr::str_glue("toppData_{gr}.xlsx")
+        } else {
+          this_file = stringr::str_glue("{filename}_{gr}.xlsx")
+        }
+
+        openxlsx::write.xlsx(tmp_toppData,
+                             file = this_file,
+                             colNames = TRUE,
+                             rowNames = FALSE,
+                             borders = "columns",
+                             sheetName="toppData",
+                             overwrite=T)
+        cat("Saving file:", this_file, "\n")
+
+      } else if (format == "csv") {
+        if (is.null(filename)) {
+          this_file = stringr::str_glue("toppData_{gr}.csv")
+        } else {
+          this_file = stringr::str_glue("{filename}_{gr}.csv")
+        }
+
+        write.csv(tmp_toppData,
+                  file = this_file,
+                  quote = FALSE,
+                  row.names = FALSE,
+                  col.names = TRUE)
+        cat("Saving file:", this_file, "\n")
+      } else if (format == "tsv") {
+      if (is.null(filename)) {
+        this_file = stringr::str_glue("toppData_{gr}.tsv")
+      } else {
+        this_file = stringr::str_glue("{filename}_{gr}.tsv")
+      }
+
+        write.table(tmp_toppData,
+                  file = this_file,
+                  sep = "\t",
+                  quote = FALSE,
+                  row.names = FALSE,
+                  col.names = TRUE)
+        cat("Saving file:", this_file, "\n")
+      }
+    }
+  } else {
+      if (format == 'xlsx') {
+
+        if (is.null(filename)) {
+          this_file = stringr::str_glue("toppData.xlsx")
+        } else {
+          this_file = stringr::str_glue("{filename}.xlsx")
+        }
+
+        openxlsx::write.xlsx(tmp_toppData,
+                             file = this_file,
+                             colNames = TRUE,
+                             rowNames = FALSE,
+                             borders = "columns",
+                             sheetName="toppData",
+                             overwrite=T)
+        cat("Saving file:", this_file, "\n")
+
+      } else if (format == "csv") {
+        if (is.null(filename)) {
+          this_file = stringr::str_glue("toppData.csv")
+        } else {
+          this_file = stringr::str_glue("{filename}.csv")
+        }
+
+        write.csv(tmp_toppData,
+                  file = this_file,
+                  quote = FALSE,
+                  row.names = FALSE,
+                  col.names = TRUE)
+        cat("Saving file:", this_file, "\n")
+      } else if (format == "tsv") {
+      if (is.null(filename)) {
+        this_file = stringr::str_glue("toppData.tsv")
+      } else {
+        this_file = stringr::str_glue("{filename}.tsv")
+      }
+
+      write.table(tmp_toppData,
+                  file = this_file,
+                  sep = "\t",
+                  quote = FALSE,
+                  row.names = FALSE,
+                  col.names = TRUE)
+      cat("Saving file:", this_file, "\n")
+    }
+
+  }
+
+}
+
--- R/topp_plot.R
+++ R/topp_plot.R
@@ -9,8 +9,15 @@
 #' @importFrom dplyr filter mutate
 #' @importFrom stringr str_wrap
 #' @importFrom forcats fct_reorder
+#' @importFrom viridis scale_color_viridis
 #' @export
-toppPlot <- function (toppData, category = NULL, clusters = NULL, p_val_adj="BH") {
+toppPlot <- function (toppData,
+                      category = NULL,
+                      clusters = NULL,
+                      p_val_adj="BH",
+                      save = FALSE,
+                      save_dir = NULL,
+                      width = 7) {
   #check toppData object - check for columns? - check that clusters and category are in data
   test_cols = c("Category", "Name", "PValue", "GenesInTerm", "GenesInQuery", "GenesInTermInQuery")
   for (t in test_cols) {
@@ -19,7 +26,7 @@ toppPlot <- function (toppData, category = NULL, clusters = NULL, p_val_adj="BH"
     }
   }
   #parse category
-  if (is.null(category)){
+  if (!(is.null(category))){
     if (length(unique(toppData$Category)) > 1) {
       stop("Please select a ToppFun category to plot (e.g. category = 'GeneOntologyMolecularFunction').")
     }
@@ -33,10 +40,13 @@ toppPlot <- function (toppData, category = NULL, clusters = NULL, p_val_adj="BH"
 
   #parse pvalue
 
-  if (!(p_val_adj %in% c("BH", "Bonferroni", "BY", "none", "None"))) {
+  if (!(p_val_adj %in% c("BH", "Bonferroni", "BY", "none", "None", "log"))) {
     cat("P value adjustment not found - using 'BH' by default. For no adjustment, use p_val_adj = 'none'.")
   }
 
+  if ("nlog10_fdr" %in% colnames(toppData)) {
+    p_val_adj = "log"
+  }
 
   p_val_col = switch(p_val_adj,
                      "BH" = "QValueFDRBH",
@@ -44,11 +54,24 @@ toppPlot <- function (toppData, category = NULL, clusters = NULL, p_val_adj="BH"
                      "BY" = "QvalueFDRBY",
                      "none" = "PValue",
                      "None" = "PValue",
+                     "log" = "nlog10_fdr",
                      "QValueFDRBH")
+
   if (p_val_col %in% c("QValueFDRBH", "QValueBonferroni", "QvalueFDRBY")) {
     color_label = "Adj. P-value"
+  } else if (p_val_col == "nlog10_fdr") {
+    color_label = "-Log10(FDR)"
   } else {
     color_label = "P-value"
+  }
+
+  #parse save
+  if (isTRUE(save)) {
+    if (is.null(save_dir)) {
+      output_dir = getwd()
+    } else {
+      output_dir = save_dir
+    }
   }
 
   #parse clusters
@@ -57,58 +80,178 @@ toppPlot <- function (toppData, category = NULL, clusters = NULL, p_val_adj="BH"
       clusters = unique(toppData$Cluster)
     }
   }
+  print(clusters)
+  print(length(clusters))
   if (length(clusters) > 1) {
     cat("Multiple clusters entered: function returns a list of ggplots")
+
     plot_list = list()
-    for (c in clusters) {
-      plot_list[[c]] <- toppData |>
-        dplyr::filter(Cluster == c) |>
-        dplyr::filter(Category == category) |>
-        dplyr::mutate(geneRatio = GenesInTermInQuery / GenesInTerm) |>
-        dplyr::mutate(Name = forcats::fct_reorder(Name, geneRatio)) |>
-        ggplot(mapping = aes(
-          x = geneRatio,
-          y = Name
-        )) +
-        geom_segment(aes(xend=0, yend=Name)) +
-        geom_point(mapping = aes(size=GenesInTermInQuery, color=PValue)) +
-        scale_color_gradient(low='red', high = 'gray') +
-        theme_bw() +
-        scale_y_discrete(labels = function(x) stringr::str_wrap(x, width = 20, whitespace_only = F)) +
-        labs(color=color_label, size = "Genes from Query in Gene Set")
+    for (cat in category) {
+      for (c in clusters) {
+        plot_list[[c]] <- toppData |>
+          dplyr::filter(Cluster == c) |>
+          dplyr::filter(Category == category) |>
+          dplyr::mutate(geneRatio = GenesInTermInQuery / GenesInTerm) |>
+          dplyr::mutate(Name = forcats::fct_reorder(Name, geneRatio)) |>
+          ggplot(mapping = aes(
+            x = geneRatio,
+            y = Name
+          )) +
+          geom_segment(aes(xend=0, yend=Name)) +
+          geom_point(mapping = aes(size=GenesInTermInQuery, color=!!as.name(p_val_col))) +
+          viridis::scale_color_viridis()  +
+          theme_bw() +
+          scale_y_discrete(labels = function(x) stringr::str_wrap(x, width = 20, whitespace_only = F)) +
+          labs(color=color_label, size = "Genes from Query\n in Gene Set")
+
+        if (isTRUE(save)) {
+          filename = str_glue("{cat}_{c}_toppDotPlot.pdf")
+          ggplot2::ggsave(filename = file.path(output_dir, filename),
+                          width = width)
+        }
+      }
     }
     return (plot_list)
-  } else if ("Cluster" %in% colnames(toppData)) {
-    toppData |>
-      dplyr::filter(Cluster == clusters) |>
-      dplyr::filter(Category == category) |>
-      dplyr::mutate(geneRatio = GenesInTermInQuery / GenesInTerm) |>
-      dplyr::mutate(Name = forcats::fct_reorder(Name, geneRatio)) |>
-      ggplot(mapping = aes(
-        x = geneRatio,
-        y = Name
-      )) +
-      geom_segment(aes(xend=0, yend=Name)) +
-      geom_point(mapping = aes(size=GenesInTermInQuery, color=PValue)) +
-      scale_color_gradient(low='red', high = 'gray') +
-      theme_bw() +
-      scale_y_discrete(labels = function(x) str_wrap(x, width = 20, whitespace_only = F)) +
-      labs(color=color_label, size = "Genes from Query in Gene Set")
   } else {
-    toppData |>
-      dplyr::filter(Category == category) |>
-      dplyr::mutate(geneRatio = GenesInTermInQuery / GenesInTerm) |>
-      dplyr::mutate(Name = forcats::fct_reorder(Name, geneRatio)) |>
-      ggplot(mapping = aes(
-        x = geneRatio,
-        y = Name
-      )) +
-      geom_segment(aes(xend=0, yend=Name)) +
-      geom_point(mapping = aes(size=GenesInTermInQuery, color=PValue)) +
-      scale_color_gradient(low='red', high = 'gray') +
-      theme_bw() +
-      scale_y_discrete(labels = function(x) str_wrap(x, width = 20, whitespace_only = F)) +
-      labs(color=color_label, size = "Genes from Query in Gene Set")
-  }
+      for (cat in category) {
+        for (c in clusters) {
+          toppData |>
+            dplyr::filter(Cluster == c) |>
+            dplyr::filter(Category == category) |>
+            dplyr::mutate(geneRatio = GenesInTermInQuery / GenesInTerm) |>
+            dplyr::mutate(Name = forcats::fct_reorder(Name, geneRatio)) |>
 
+            ggplot(mapping = aes(
+              x = geneRatio,
+              y = Name
+            )) +
+            geom_segment(aes(xend=0, yend=Name)) +
+            geom_point(mapping = aes(size=GenesInTermInQuery, color=!!as.name(p_val_col))) +
+            viridis::scale_color_viridis()  +
+            theme_bw() +
+            scale_y_discrete(labels = function(x) stringr::str_wrap(x, width = 20, whitespace_only = F)) +
+            labs(color=color_label, size = "Genes from Query\n in Gene Set")
+
+          if (isTRUE(save)) {
+            filename = str_glue("{cat}_{c}_toppDotPlot.pdf")
+            ggplot2::ggsave(filename = file.path(output_dir, filename),
+                            width = width)
+          }
+        }
+      }
+  }
 }
+
+#' Create a balloon plot from toppdata results
+#'
+#' @param toppData A toppData results dataframe
+#' @param categories The topp categories to plot
+#' @param balloons Number of balloons per group to plot
+#' @param filename Filename of the saved balloon plot
+#' @param save Save the balloon plot if TRUE
+#' @param height Height of the saved balloon plot
+#' @param width Width of the saved balloon plot
+#' @return ggplot
+#' @importFrom ggplot2 ggplot geom_point theme scale_x_discrete theme_bw labs aes xlab ggsave
+#' @importFrom dplyr group_by mutate slice_max
+#' @importFrom stringr str_wrap str_glue
+#' @importFrom forcats fct_reorder
+#' @importFrom viridis scale_color_viridis
+#' @export
+toppBalloon <- function (toppData,
+                         categories = NULL,
+                         balloons = 3,
+                         filename = NULL,
+                         save = TRUE,
+                         height = 5,
+                         width = 10) {
+
+  if (is.null(categories)) {
+    categories <- unique(toppData[["Category"]])
+  }
+  balloon <- list()
+  for (cat in categories) {
+    cat("Balloon Plot:", cat)
+  balloon[[cat]] <- toppData |>
+    dplyr::filter(Category == cat) |>
+    dplyr::group_by(Cluster) |>
+    dplyr::slice_max(order_by=-nlog10_fdr, n=balloons, with_ties = F) |>
+    dplyr::mutate(geneRatio = GenesInTermInQuery / GenesInTerm) |>
+    ggplot(aes(
+      x=forcats::fct_reorder(Name, Cluster),
+      y=Cluster,
+    )) +
+    ggplot2::geom_point(aes(size=geneRatio, color=nlog10_fdr)) +
+    viridis::scale_color_viridis(option = "C") +
+    ggplot2::labs(color="FDR", size="Genes") +
+    ggplot2::xlab(cat) +
+    ggplot2::theme_bw() +
+    ggplot2::scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 30, whitespace_only = F)) +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(size = 6, angle = 60, hjust=1.1, vjust=1.05),
+                   panel.border = ggplot2::element_rect(color = NA))
+
+    if (is.null(filename)) {
+      plot_filename <- stringr::str_glue("toppBalloon_{cat}.pdf")
+    } else {
+      plot_filename = stringr::str_glue("{filename}_{cat}.pdf")
+    }
+    if (isTRUE(save)){
+      ggplot2::ggsave(plot_filename, height = height, width = width)
+      cat(" saved to:", plot_filename)
+    }
+  cat("\n")
+
+  }
+  if (length(balloon) == 1) {
+    print(balloon[1])
+  }
+  return (balloon)
+}
+
+
+# else if ("Cluster" %in% colnames(toppData)) {
+#     toppData |>
+#       dplyr::filter(Cluster == clusters) |>
+#       dplyr::filter(Category == category) |>
+#       dplyr::mutate(geneRatio = GenesInTermInQuery / GenesInTerm) |>
+#       dplyr::mutate(Name = forcats::fct_reorder(Name, geneRatio)) |>
+#       ggplot(mapping = aes(
+#         x = geneRatio,
+#         y = Name
+#       )) +
+#       geom_segment(aes(xend=0, yend=Name)) +
+#       geom_point(mapping = aes(size=GenesInTermInQuery, color=!!as.name(p_val_col))) +
+#       viridis::scale_color_viridis()  +
+#       theme_bw() +
+#       scale_y_discrete(labels = function(x) str_wrap(x, width = 20, whitespace_only = F)) +
+#       labs(color=color_label, size = "Genes from Query\n in Gene Set")
+#     if (isTRUE(save)) {
+#       filename = str_glue("{cat}_{c}_toppDotPlot.pdf")
+#       ggplot2::ggsave(filename = file.path(output_dir, filename),
+#                       width = width)
+#     }
+#   } else {
+#     toppData |>
+#       dplyr::filter(Category == category) |>
+#       dplyr::mutate(geneRatio = GenesInTermInQuery / GenesInTerm) |>
+#       dplyr::mutate(Name = forcats::fct_reorder(Name, geneRatio)) |>
+#       ggplot(mapping = aes(
+#         x = geneRatio,
+#         y = Name
+#       )) +
+#       geom_segment(aes(xend=0, yend=Name)) +
+#       geom_point(mapping = aes(size=GenesInTermInQuery, color=!!as.name(p_val_col))) +
+#       viridis::scale_color_viridis() +
+#       theme_bw() +
+#       scale_y_discrete(labels = function(x) str_wrap(x, width = 20, whitespace_only = F)) +
+#       labs(color=color_label, size = "Genes from Query in Gene Set")
+#     if (isTRUE(save)) {
+#       filename = str_glue("{category}_{unique}_toppDotPlot.pdf")
+#       ggplot2::ggsave(filename = file.path(output_dir, filename),
+#                       width = width)
+#     }
+#   }
+#
+# }
+#
+#
